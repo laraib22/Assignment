8ac2481da64d9e424e2d0376940d479bc19f3762
fatimasadiqa <fatima2019@namal.edu.pk>
2021-10-04 21:52:20 +0500

    add function

--- cal01.py
+++ cal01.py
@@ -1,7 +1,7 @@
 def add(num1, num2):
    return num1 + num2
-def squAre(num1,num2):
-   return num1**num2
-res = (add(5, 3),squAre(5,3))
+def sub(num1,num2):
+   return num1-num2
+res = (add(5, 3),sub(6,2))
 
 print(res)
